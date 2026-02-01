eab86b5bbb0c74eb65feef9331b8a86b8a4f34b8
Brian Lara <Briandlara1@gmail.com>
2026-02-01 12:53:43 -0500

    removced unused imports

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "react-oidc-context";
 
